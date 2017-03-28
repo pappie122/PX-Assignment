9fcade9755811749b7ae884a4cf306b53f1ad052
pappie122 <concrete_window@hotmail.com>
2017-03-28 19:40:03 +1100

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,43 @@
+<div id="container" style="width:700px">
+
+<div id="header" style="background-color:#980000 ;">
+<h1 style="margin-bottom:0;">User registration</h1></div>
+
+
+
+</div>
+
+<div id="content" style="background-color:#EEEEEE;height:800px;width:400px;float:left;">
+      <div id="reg" style="align-center">
+	  <form name="myForm" method="post" action="submit.php">
+	  
+	  <tr>First Name: <input type="text" name="fname" size="20"></tr><br>
+	  <tr>lastname: <input type="text" name="lname" size="20"></tr><br>
+<tr>Address: <input type="text" name="addr" size="20"></tr><br>
+<tr>mobile: <input type="number" name="mobile" size="20"></tr><br>
+<tr>telephone: <input type="number" name="tel" size="20"></tr><br>
+	  <tr>username: <input type="text" name="usern" size="20"></tr><br>
+	  <tr>password: <input type="text" name="pwds" size="20"></tr><br>
+	  <tr>confirm: <input type="text" name="pwds2" size="20"></tr><br>
+	
+
+		
+		
+		
+		
+	  <input type="submit" value="Submit">
+	   
+
+	  
+	  </form>
+	   
+	   </div>
+	   </div>
+
+<div id="footer" style="background-color:#FFA500;clear:both;text-align:center;">
+</div>
+
+</div>
+
+</body>
+</html> 
