280e2dce957663e3d48cc9e0e3d4c2cd0394764d
Fiona MacDonald <Fiona MacDonald>
2017-04-13 14:42:18 +1000

    Made logic for timesheet still working on it

--- timesheet.php
+++ timesheet.php
@@ -35,7 +35,20 @@
 
 <table>
 <tr><th>Day of Week</th><th>start time</th><th>end time</th><th>break</th><th>Job</th></tr>
-
+<?php
+	  $myCalendar = new tc_calendar("date5", true, false);
+	  $myCalendar->setIcon("calendar/images/iconCalendar.gif");
+	  $myCalendar->setDate(date('d'), date('m'), date('Y'));
+	  $myCalendar->setPath("calendar/");
+	  $myCalendar->setYearInterval(2000, 2017);
+	  $myCalendar->dateAllow('2008-05-13', '2017-03-01');
+	  $myCalendar->setDateFormat('j F Y');
+	  $myCalendar->setAlignment('left', 'bottom');
+	  $myCalendar->setSpecificDate(array("2011-04-01", "2011-04-04", "2011-12-25"), 0, 'year');
+	  $myCalendar->setSpecificDate(array("2011-04-10", "2011-04-14"), 0, 'month');
+	  $myCalendar->setSpecificDate(array("2011-06-01"), 0, '');
+	  $myCalendar->writeScript();
+	  ?>
 <tr><td>Monday</td><td>
 <input type="text" name="Monday" size="3" maxlength="4" value=""
  onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()">
