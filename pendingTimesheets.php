3141a637139f20d6b1326bcc95be94f2760e2091
DESKTOP-0HF5GUB\Alex <sealex001@gmail.com>
2017-05-29 16:23:33 +1000

    Changed userJob field

--- pendingTimesheets.php
+++ pendingTimesheets.php
@@ -1,71 +1,62 @@
 <?php
 include("config.php");
 session_start();
-
 if(isset($_SESSION['login_user'])){
   // echo "Your session is running " . $_SESSION['login_user'];
   $email = $_SESSION['login_user'];
-
   $data = 'SELECT * FROM user WHERE Email = "'.$email.'"';
   $query = mysqli_query($conn, $data) or die("Couldn't execute query. ". mysqli_error());
   $data2 = mysqli_fetch_array($query);
+  
 }
-
-
 ?>
-
 
 <!DOCTYPE html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
 	<html lang="en">
-	<!--<head>
-		<meta charset="UTF-8">
-		<title>Pending timesheets</title>
-		<link rel="stylesheet" 
-		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-	</head> -->
+	
 	<head>
-		<meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<title>Pending timesheets</title>
+	
+		<link rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="index.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		
-		<title>Pending timesheets</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		
-			<link rel="stylesheet"
-			href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-			<link rel="stylesheet"
-			href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-			<link rel="stylesheet" href="index.css">
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			
+		<meta name="viewport" content="width=device-width, initial-scale=1">		
 	</head>
+	
 	<body>
-	
-	  <!-- <h1> Pending timesheets </h1> -->
-	<nav class="navbar navbar-default">  
+	<!-- <h1> Pending timesheets </h1> -->
+	<nav class="navbar navbar-default">
 	<?php include("nav.php");?>
-	<div class="container">
-    <div class="jumbotron">
 	
-		<!--<div class="container">-->
+	<div class="container">
+    <!-- <div class="jumbotron"> -->
+	<div class="col-sm-12 text-left">
+	<!--<div class="container">-->
 		
 
 <?php
-$conn = mysqli_connect("localhost","root",NULL,"px");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  
-    
- 	
-//process timesheet button action
+	$conn = mysqli_connect($host,$username,$password,$dbname);
+	
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+   	
+	//process timesheet button action
 	if(isset($_POST['processtimesheet'])){
+	
 		
 		$tsid = $_POST["timesheetid"]; 
 		$msg = $_POST["msg"];
@@ -79,21 +70,17 @@ if (mysqli_connect_errno())
 				echo "<script type='text/javascript'>alert('Please complete all fields.')</script>";
 				
 			} else if (!empty($msg)&&($act == "none")){
-			
+				
 				echo "<script type='text/javascript'>alert('Please select an action.')</script>";
 				
-	
 				} else if ((empty($msg))&&($act == ("none")||("3")||("1"))){
-				
+					
 					echo "<script type='text/javascript'>alert('Please enter feedback.')</script>";
 					
-	
 					}else if (!empty($msg)&&($act == "3")){
-						
 						
 						$approvedate = date('Y-m-d');
 						//Get admin ID through session & update ApprovedBy field in database
-						
 						
 							$sql = "UPDATE timesheet SET timesheetStatus = '$act', ApprovedDate = '$approvedate', ApprovedBy = '$email', Comments = '$msg' WHERE timesheet.TimesheetID = '$tsid'";
 							$rs = mysqli_query($conn, $sql)
@@ -101,15 +88,12 @@ if (mysqli_connect_errno())
 							
 							echo "<script type='text/javascript'>alert('Timesheet has been processed.')</script>";
 							
-							
 						//email function not operational
 						
 								//$to = "recipient";
 								//$subject = "Timesheet Approved";
-
 								//$message = "<b>Your timesheet has been processed and approved.</b>";
 								//$message .= "<h1>Timesheet Approved.</h1>";
-
 								//$header = "From:example@email.com \r\n";
 								//$retval = mail($to,$subject,$message,$header);
 								//if(isset($retval))//change
@@ -136,10 +120,8 @@ if (mysqli_connect_errno())
 					
 								//$to = "recipient";
 								//$subject = "Timesheet Approved";
-
 								//$message = "<b>Your timesheet has been processed and approved.</b>";
 								//$message .= "<h1>Timesheet Approved.</h1>";
-
 								//$header = "From:example@email.com \r\n";
 								//$retval = mail($to,$subject,$message,$header);
 								//if(isset($retval))//change
@@ -153,7 +135,6 @@ if (mysqli_connect_errno())
 									
 								}
 	}
-
 	
 	
 	
@@ -164,14 +145,14 @@ if (mysqli_connect_errno())
 	
 	
 	
-//view details button action
-		  if(isset($_POST['viewdetails'])){
-			  				
-			 
+	//view details button action
+	if(isset($_POST['viewdetails'])){
+		
+		
+		
+		
 			$id = $_POST["userid"];
 			$ts = $_POST["timesheetid"];
-			
-						
 				
 			$sql = "SELECT Email, TsDetailID, timesheetdetail.TimesheetID, timesheetdetail.JobID, job.JobName, timesheetdetail.Date, StartTime, EndTime, BreakDuration, timesheetdetail.TotalHours, timesheetdetail.Comments FROM timesheetdetail 
 			INNER JOIN timesheet ON timesheetdetail.TimesheetID = timesheet.TimesheetID	
@@ -181,84 +162,69 @@ if (mysqli_connect_errno())
 			
 			$rs2 = mysqli_query($conn, $sql)
 			or die (mysqli_error($conn));
-				
+			
 				
 				if (mysqli_num_rows($rs2)> 0 ) { ?>
+				
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 				
 					<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
 					<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>	
 					
-							<!-- Create Table -->
-							<table class="table table-hover" id="tab_logic">
-							
-								<tr>
-									<th bgcolor="lightblue">Job Name</th>
-									<th bgcolor="lightblue">Date</th>
-									<th bgcolor="lightblue">Start Time</th>
-									<th bgcolor="lightblue">End Time</th>
-									<th bgcolor="lightblue">Break</th>
-									<th bgcolor="lightblue">Hours</th>
-									<th bgcolor="lightblue">Comments</th>
-								</tr>
-							
-							
+						<!-- Create Table -->
+						<table class="table table-hover" id="tab_logic">
+						
+							<tr>
+								<th bgcolor="lightblue">Job Name</th>
+								<th bgcolor="lightblue">Date</th>
+								<th bgcolor="lightblue">Start Time</th>
+								<th bgcolor="lightblue">End Time</th>
+								<th bgcolor="lightblue">Break</th>
+								<th bgcolor="lightblue">Hours</th>
+								<th bgcolor="lightblue">Comments</th>
+							</tr>
 		  <?php
-										// fetch data while results > 0
-										while ($row = mysqli_fetch_array($rs2)) { ?>
-										
-										<form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
-											
-													<tr>
-														<td><?php echo $row["JobName"]?></td>
-															<?php //Change format of date
-																$changeDate = $row["Date"];
-																$newDate=date("d/m/Y", strtotime($changeDate));
-															?>
-														<td><?php echo $newDate?></td>
-														<td><?php echo $row["StartTime"]?></td>
-														<td><?php echo $row["EndTime"]?></td>
-														<td><?php echo $row["BreakDuration"]?></td>
-														<td><?php echo $row["TotalHours"]?></td>
-														<td><?php echo $row["Comments"]?></td>
-											<?php echo "<td>" . "<input type=hidden name=emailaddress value=" . $row['Email'] . " </td>";?>
-											<?php echo "<td>" . "<input type=hidden name=timesheetid value=" . $row['TimesheetID'] . " </td>";?>
-											
-															
-													</tr>
-												
-												
-										
-											
-											<?php } ?> 
-								
-								
-								
+						// fetch data while results > 0
+						
+						while ($row = mysqli_fetch_array($rs2)) { ?>
+						
+						<form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
+							
+							<tr>
+								<td><?php echo $row["JobName"]?></td>
+									<?php //Change format of date
+										$changeDate = $row["Date"];
+										$newDate=date("d/m/Y", strtotime($changeDate));
+									?>
+								<td><?php echo $newDate?></td>
+								<td><?php echo $row["StartTime"]?></td>
+								<td><?php echo $row["EndTime"]?></td>
+								<td><?php echo $row["BreakDuration"]?></td>
+								<td><?php echo $row["TotalHours"]?></td>
+								<td><?php echo $row["Comments"]?></td>
+								<?php echo "<td>" . "<input type=hidden name=emailaddress value=" . $row['Email'] . " </td>";?>
+								<?php echo "<td>" . "<input type=hidden name=timesheetid value=" . $row['TimesheetID'] . " </td>";?>		
+							</tr>				
+						<?php } ?> 
+									
 							</table>
 							
-							
-							
-								<label>Enter Feedback</label>
 								
+								<h5> Enter Feedback </h5>
 									<p><textarea name="msg" ></textarea></p>
 									
-									<label>Action</label>
-									
+								<h5> Choose Action </h5>
 									<p><select name="action"> 
 											<option value="none" select="selected">  </option>
 											<option value="3"> Approve </option>											
 											<option value="1"> Reject </option>
-	
-											
 									</select></p>
 									
-									<p><input name="processtimesheet" type="submit" value="Apply Changes"/></p>
+								<p><input name="processtimesheet" type="submit" value="Apply Changes"/></p>
 									
-								</form>	
-								
-									
-									
-									
-								
+						</form>	
+																																								
 			<?php } else {
 							echo "no timesheet details available";
 						} 
@@ -266,19 +232,13 @@ if (mysqli_connect_errno())
 				ob_flush();
 				flush();
 		
-//if view detials button not selected display pending timesheets
+	//if view detials button not selected display pending timesheets
 	} else { ?>
  
- 
- 
- 
- <!-- original place to start html doc type -->
-
-	
-		
+ <!-- original place to start html doc type 
+	nav page -->
 			
 	<?php		
-
 			$sql = "SELECT CONCAT (Fname,' ', Lname) AS Fullname, Email,DateCreated,DateSubmitted,TimesheetStatus,TotalHours,Comments,ApprovedDate,ApprovedBy,timesheet.UserID,TimesheetID FROM timesheet INNER JOIN
 			user on timesheet.UserID = user.UserID WHERE TimesheetStatus = 2";
 			$rs = mysqli_query($conn, $sql)
@@ -385,6 +345,7 @@ if (mysqli_connect_errno())
 				
 				<?php } ?>
 				</table>
+				</div>
 				<?php
 				
 			//if no pending timesheets exist
@@ -400,10 +361,10 @@ if (mysqli_connect_errno())
 				
 			
 			
-		<!-- </div> -->
-		
-	</div>
-	</div>
+	</div> 
+	<!-- </div> -->
+	<!-- </div> -->
+	
   <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
@@ -429,6 +390,7 @@ if (mysqli_connect_errno())
 		
 		
 	</script>
+	
 	</body>
 	</html>
   <?php } ?>
